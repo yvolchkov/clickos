12e8d59f1b5744b86250d456a176ffde6a5623ef
Joao Martins <Joao.Martins@neclab.eu>
2014-03-15 01:48:12 +0100

    Packet: Increase default_headroom under CLICK_OS
    Improve performance by reduce the number of memory reallocations.

--- include/click/packet.hh
+++ include/click/packet.hh
@@ -36,8 +36,12 @@ class Packet { public:
     // PACKET CREATION
 
     enum {
+#ifdef CLICK_OS
+	default_headroom = 48,		///< Increase headroom for improved performance.
+#else
 	default_headroom = 28,		///< Default packet headroom() for
 					///  Packet::make().  4-byte aligned.
+#endif
 	min_buffer_length = 64		///< Minimum buffer_length() for
 					///  Packet::make()
     };
